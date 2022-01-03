dc21b9232c7a083a40b3337dff47d96c05485c18
bycho91 <bycho1991@gmail.com>
2022-01-03 15:26:09 -0600

    changes to reset button and error alerts

--- app.js
+++ app.js
@@ -1,66 +1,81 @@
-const bill = document.querySelector('#input-bill');
-const customTip = document.querySelector('#input-custom-tip');
-const people = document.querySelector('#input-people');
-const resultTip = document.querySelector('#tip-amt');
-const resultBill = document.querySelector('#total-amt');
+const bill = document.querySelector("#input-bill");
+const customTip = document.querySelector("#input-custom-tip");
+const people = document.querySelector("#input-people");
+const resultTip = document.querySelector("#tip-amt");
+const resultBill = document.querySelector("#total-amt");
 let tip;
 
 // tip per person = bill * tip% / people
 // bill per person = bill * (1+tip%) / people
 
 // add btn-active class whenever a btn is clicked and remove from previous
-document.querySelector('.btns-wrapper').addEventListener('click', (e) => {
-    const toRemove = document.querySelector('.btn-active');
-    if(e.target.classList.contains('btn')) {
-        if (toRemove) toRemove.classList.remove('btn-active');
-        e.target.classList.add('btn-active');
-    }
-})
+document.querySelector(".btns-wrapper").addEventListener("click", (e) => {
+  const toRemove = document.querySelector(".btn-active");
+  if (e.target.classList.contains("btn")) {
+    if (toRemove) toRemove.classList.remove("btn-active");
+    e.target.classList.add("btn-active");
+  }
+});
+
+const validateFloat = (s) => {
+  let rgx = /^[0-9]*\.?[0-9]*$/;
+  return s.match(rgx);
+};
+
+const removeErrorMessage = (targetElement) => {
+  setTimeout(() => {
+    targetElement.style.display = "none";
+  }, 3000);
+};
 
 const calculate = async () => {
-    if(parseInt(people.value) <= 0) {
-        document.querySelector('.alert-error').style.display = 'block';
-        return;
-    }
-    let inputTipEl = await document.querySelector('.btn-active');
-    
-    if(inputTipEl.classList.contains('custom-btn')) {
-        tip = parseInt(inputTipEl.value)/100;
-    }else {
-        tip = parseInt(inputTipEl.innerText)/100;
-    }
+  // this validates whether the # of People is greater than 0
+  if (parseFloat(people.value) <= 0) {
+    document.querySelector(".error-people").style.display = "block";
+    removeErrorMessage(document.querySelector(".error-people"));
+    return;
+  }
 
-    const tipPerPerson = (parseInt(bill.value)*tip)/parseInt(people.value)
-    const billPerPerson = (parseInt(bill.value) + tipPerPerson)/parseInt(people.value);
+  // validate the bill for real values
+  if (!validateFloat(bill.value)) {
+    document.querySelector(".error-bill").style.display = "block";
+    removeErrorMessage(document.querySelector(".error-bill"));
+    return;
+  }
 
-    return {
-        tipPerPerson,
-        billPerPerson,
-    }
-}
+  let inputTipEl = await document.querySelector(".btn-active");
 
-// document.addEventListener('keydown', e => {
-//     if(e.key === 'Enter') {
-//         calculate().then(({tipPerPerson, billPerPerson}) => {
-//             resultTip.innerText = `$${tipPerPerson.toFixed(2)}`;
-//             resultBill.innerText = `$${billPerPerson.toFixed(2)}`;
-//         })
-//     }
-// })
+  if (inputTipEl.classList.contains("custom-btn")) {
+    tip = parseFloat(inputTipEl.value) / 100;
+  } else {
+    tip = parseFloat(inputTipEl.innerText) / 100;
+  }
 
-document.querySelector('#split-btn').addEventListener('click', () => {
-    calculate().then(({tipPerPerson, billPerPerson}) => {
-        resultTip.innerText = `$${tipPerPerson.toFixed(2)}`;
-        resultBill.innerText = `$${billPerPerson.toFixed(2)}`;
-    })
-})
+  const tipPerPerson = (parseFloat(bill.value) * tip) / parseInt(people.value);
+  const billPerPerson =
+    (parseFloat(bill.value) + tipPerPerson) / parseInt(people.value);
 
-document.querySelector('#reset-btn').addEventListener('click', () => {
-    bill.value = '';
-    customTip.value = '';
-    people.value = '';
-    resultTip.innerText = '$0.00';
-    resultBill.innerText = '$0.00';
-    document.querySelector('.alert-error').style.display = 'none';
-})
+  return {
+    tipPerPerson,
+    billPerPerson,
+  };
+};
 
+// Event listener for the split button
+document.querySelector("#split-btn").addEventListener("click", () => {
+  calculate().then(({ tipPerPerson, billPerPerson }) => {
+    resultTip.innerText = `$${tipPerPerson.toFixed(2)}`;
+    resultBill.innerText = `$${billPerPerson.toFixed(2)}`;
+  });
+});
+
+// Resets the calculator
+document.querySelector("#reset-btn").addEventListener("click", () => {
+  bill.value = "";
+  customTip.value = "";
+  people.value = "";
+  resultTip.innerText = "$0.00";
+  resultBill.innerText = "$0.00";
+  document.querySelector(".alert-error").style.display = "none";
+  document.querySelector(".btn-active").classList.remove("btn-active");
+});
